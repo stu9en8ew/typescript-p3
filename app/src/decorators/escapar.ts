85ab4da9843f2ccf0ee54724f85b8b4c5614c4e4
Márcio Marques <mar.info.rj@gmail.com>
2025-12-26 17:22:29 +0100

    nuovo decorator escapar

---
+++ app/src/decorators/escapar.ts
@@ -0,0 +1,20 @@
+export function escapar(
+    target: any,
+    propertyKey: String,
+    descriptor: PropertyDescriptor
+    ){
+        const metodoOriginal = descriptor.value;
+        descriptor.value = function (...args : any[]) {
+            let retorno = metodoOriginal.apply(this, args);
+            if(typeof retorno === 'string'){
+                /*
+                console.log(`@escape eme acao na classe 
+                    ${this.constructor.name} para o metodo ${propertyKey}`)
+                    */
+                retorno = retorno
+                .replace(/<script>[\s\S]*?<\/script>/, '');
+            }
+            return retorno;    
+        }
+        return descriptor;
+    }
