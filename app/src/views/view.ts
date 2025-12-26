6b7a04338d7c02f9ac7922dd9062e3de1aeeabbc
Márcio Marques <mar.info.rj@gmail.com>
2025-12-26 16:45:01 +0100

    esecuzione di due decorators

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,3 +1,4 @@
+import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 
 export abstract class View<T> {
@@ -18,6 +19,7 @@ export abstract class View<T> {
     }
 
     @logarTempoDeExecucao(true)
+    @inspect()
     public update(model: T): void {
         let template = this.template(model);
         if (this.escapar) {
